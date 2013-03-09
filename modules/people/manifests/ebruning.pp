536695d86e1e01125b3f0dfe4c073b4194292a6c
Ethan Bruning <ebruning@gmail.com>
2013-03-08 22:59:34 -0800

    Add personal manifest

---
+++ modules/people/manifests/ebruning.pp
@@ -0,0 +1,15 @@
+class people::ebruning {
+	include dropbox
+	include adium
+	include chrome
+	include sublime_text_2
+	include mou
+	include iterm2::stable
+	include zsh
+	include onepassword
+	include handbrake
+	#include vlc
+	#include heroku
+	#include istatmenus4
+
+}
