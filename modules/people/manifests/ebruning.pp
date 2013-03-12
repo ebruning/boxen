30f4c5ec0c2f6de83b32cf27a1aeebcaee5c5610
Ethan Bruning <ebruning@gmail.com>
2013-03-12 13:40:36 -0700

    Refactor add modules

--- modules/people/manifests/ebruning.pp
+++ modules/people/manifests/ebruning.pp
@@ -1,25 +1,27 @@
 class people::ebruning {
-	include dropbox
-	include adium
-	include chrome
-	include sublime_text_2
-	include mou
-	include iterm2::stable
-	include zsh
-	include onepassword
-	include handbrake
-	include java
-	include spotify
+    include dropbox
+    include adium
+    include chrome
+    include sublime_text_2
+    include mou
+    include iterm2::stable
+    include zsh
+    include onepassword
+    include handbrake
+    include java
+    include spotify
+    include mailplane::stable
+    include cyberduck
 
-	class { 'intellij':
-  	edition => 'ultimate',
-	}
+    class { 'intellij':
+    edition => 'ultimate',
+    }
 
-	#heroku::plugin { 'accounts':
-  #		source => 'ddollar/heroku-accounts'
-	#}
+    #heroku::plugin { 'accounts':
+        #source => 'ddollar/heroku-accounts'
+    #}
 
-	#include vlc
-	#include istatmenus4
-
+    #include vlc
+    #include istatmenus4
+    #include github_for_mac
 }
