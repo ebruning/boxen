fdc09d7a60b5d13441f93e7d1c7b3aa9e0821285
Ethan Bruning <ebruning@gmail.com>
2013-04-06 13:40:41 -0700

    Remove adium and Cyberduck. Organized modules. Add logging messages

--- modules/people/manifests/ebruning.pp
+++ modules/people/manifests/ebruning.pp
@@ -1,39 +1,68 @@
 class people::ebruning {
-  notice ( "Bootstrapping for ${::luser}" )
+  notice ("Bootstrapping for ${::luser}")
 
+  notice ("Default packages")
+  notice ("Installing Dropbox")
   include dropbox
-  include adium
+
+  notice ("Installing Chrome")
   include chrome
+  
+  notice ("Installing Sublime Text 2")
   include sublime_text_2
+
+  notice ("Installing Mou")
   include mou
+  
+  notice ("Installing iTerm")
   include iterm2::stable
+
+  notice ("Installing ZSH")
   include zsh
+
+  notice ("Installing 1Password")
   include onepassword
+
+  notice ("Installing Handbrake")
   include handbrake
+
+  notice ("Installing Java")
   include java
+
+  notice ("Installing Spotify")
   include spotify
-  include cyberduck
+
+  notice ("Installing Vim")
   include vim
+
+  notice ("Installing Firefox")
   include firefox
 
+  notice ("Installing Intelli-J")
   class { 'intellij':
     edition => 'ultimate',
   }
 
-  notify { "Installing specific applications for ${::hostname}": }  
+  notice ("Installing specific applications for ${::hostname}")  
   case $::hostname {
     'ebruning-imac': {
+      notice ("Installing Crashplan")
       include crashplan
+
+      notice ("Installing Mailplane")
       include mailplane::beta
     }
     
     'ebruning-mbp': { 
+      notice ("Installing Mailplane")
       include mailplane::stable   
+
+      notice ("Installing XQuartz")
       include xquartz  
     }
     
     default: {
-      notify { "Error: Unknown hostname": }
+      notice ("Unknown hostname")
     }
   }
 
